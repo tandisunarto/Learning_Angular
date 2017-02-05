20378d2c2a41e76f705f67bd53f6c6e3305ebb7f
tandisunarto <tandi.sunarto@hotmail.com>
2017-02-04 21:52:10 -0500

    Routing

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -2,9 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
 
@@ -15,11 +19,20 @@ import { StarComponent } from './shared/star.component';
     ProductListComponent,
     ProductFilterPipe,
     StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
