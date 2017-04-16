1c2d18f29fbbfc05fef88e10ab8ea8fd7dfdea6c
tandisunarto <tandi.sunarto@hotmail.com>
2017-04-15 22:52:20 -0400

    correcting angular material references

--- first-routing-app/src/app/app.module.ts
+++ first-routing-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { AdminModule } from "app/admin/admin.module";
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule,
     AppRoutingModule,
     PageModule,
     AdminModule,
