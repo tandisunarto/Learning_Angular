69b25fa3461288859e35f434f4973b04f86b5bee
Tandi Sunarto <tandisunarto@outlook.com>
2017-05-30 17:28:03 -0400

    add another component to bootstrap in app.module

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BindingModule } from './binding/binding.module';
 import { AppComponent } from './app.component';
 import { DirectivesModule } from './directives/directives.module';
 import { TemperatureModule } from 'app/services/temperature.module';
+import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -21,6 +23,6 @@ import { TemperatureModule } from 'app/services/temperature.module';
     TemperatureModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HeaderComponent]
 })
 export class AppModule { }
