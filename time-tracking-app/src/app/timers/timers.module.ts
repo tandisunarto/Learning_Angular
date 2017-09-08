0dd4cc74cb33e3236d92f4c242eba77d0d0c06fe
tandisunarto <tandi.sunarto@hotmail.com>
2017-09-07 23:21:18 -0400

    write more codes

--- time-tracking-app/src/app/timers/timers.module.ts
+++ time-tracking-app/src/app/timers/timers.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimersDashboardComponent } from './timers-dashboard.component';
 import { EditableTimerListComponent } from './editable-timers/editable-timer-list.component';
+import { ToggleableTimerFormComponent } from './toggleable-timers/toggleable-timer-form.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TimersDashboardComponent, EditableTimerListComponent]
+  declarations: [
+    TimersDashboardComponent,
+    EditableTimerListComponent,
+    ToggleableTimerFormComponent
+  ],
+  exports: [
+    TimersDashboardComponent
+  ]
 })
 export class TimersModule { }
---
+++ time-tracking-app/src/app/timers/toggleable-timers/toggleable-timer-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toggleable-timer-form',
+  templateUrl: './toggleable-timer-form.component.html',
+  styles: []
+})
+export class ToggleableTimerFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
